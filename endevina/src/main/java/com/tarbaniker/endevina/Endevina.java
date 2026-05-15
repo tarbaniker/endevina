a61eb4558094ff93f65eb04ff849a526068f2a3b
tarbaniker <enric.sanchez.duxans@gmail.com>
2026-05-15 17:31:54 +0200

    Primera prova de demanar animal, llegir l'animal entrat i mostrar l'animal llegit. Són proves trivials per comprovar que s'actualitza el Github.

--- endevina/src/main/java/com/tarbaniker/endevina/Endevina.java
+++ endevina/src/main/java/com/tarbaniker/endevina/Endevina.java
@@ -4,6 +4,8 @@
 
 package com.tarbaniker.endevina;
 
+import java.util.Scanner;  // Import the Scanner class
+
 /**
  *
  * @author enric
@@ -12,5 +14,11 @@ public class Endevina {
 
     public static void main(String[] args) {
         System.out.println("Hola Mon!");
+        
+    Scanner myObj = new Scanner(System.in);  // Create a Scanner object
+    System.out.println("Entra animal ");
+
+    String animalName = myObj.nextLine();  // Read user input
+    System.out.println("\nanimal és: " + animalName);  // Output user input
     }
 }
