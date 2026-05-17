0c481dbe0e8453b2e46c9b4128133b59f688eea9
tarbaniker <enric.sanchez.duxans@gmail.com>
2026-05-18 01:03:03 +0200

    Creada classe connectaBD per gestionar accessos a la base de dades

--- endevina/src/main/java/com/tarbaniker/endevina/connectaBD.java
+++ endevina/src/main/java/com/tarbaniker/endevina/connectaBD.java
@@ -4,6 +4,7 @@
  */
 package com.tarbaniker.endevina;
 
+import java.sql.Connection;
 import java.sql.DriverManager;
 import java.sql.SQLException;
 
@@ -13,22 +14,25 @@ import java.sql.SQLException;
  */
 public class connectaBD {
     
-    public static void obreBD() {
+    private static final String URL = "jdbc:sqlite:endevina.db";
+    
+    public  void creaBD() {
         /* Obertura/Creació de la base de dades SQLITE */
-        var url = "jdbc:sqlite:endevina.db";
+        var url = URL;
 
-        try (var conn = DriverManager.getConnection(url)) {
+        try ( var conn = DriverManager.getConnection(url)) {
             if (conn != null) {
                 var meta = conn.getMetaData();
                 System.out.println("El nom del driver és " + meta.getDriverName());
-                System.out.println("S'ha obert/creat una nova base de dades.");
+                System.out.println("S'ha obert/creat la base de dades.");
       
                 /* Creació taula animals */
                 var sql = "CREATE TABLE IF NOT EXISTS animals("
                         + "   id INTEGER PRIMARY KEY,"
                         + "   nom text NOT NULL,"
                         + "   pregunta text,"
-                        + "   apuntaId INTEGER"
+                        + "   idSI INTEGER,"
+                        + "   idNO INTEGER"
                         + ");" ;
                 try ( var stmt = conn.createStatement() ) {
                       stmt.execute(sql) ;
@@ -36,10 +40,94 @@ public class connectaBD {
                     } catch (SQLException e) {
                         System.err.println(e.getMessage());
                     }
-            }
+             }
         } catch (SQLException e) {
             System.err.println(e.getMessage());
         }
-}
+    }
     
+    public  void llegirBD(int id) {
+        System.out.printf("id rebut %d\n", id);
+        var url = URL;
+        var sql = "SELECT nom, pregunta, idSI, idNO FROM animals WHERE id = ?";
+
+        try( var conn = DriverManager.getConnection(url);
+             var stmt = conn.prepareStatement(sql) ) {
+           //Passem com a paràmetre el id rebut
+            stmt.setInt(1, id);
+            // Recuperem els resultats; id és PK pertant només un
+            var rs = stmt.executeQuery() ;
+            // de moment només faint printf per veure que em funciona el SELECT
+            System.out.printf("nom ->%s<-",rs.getString("nom"));
+            System.out.printf("pregunta ->%s<-",rs.getString("pregunta"));
+            System.out.printf("id SI %d\n", rs.getInt("idSI"));
+            System.out.printf("id NO %d\n", rs.getInt("idNO"));
+              
+        } catch (SQLException e) {
+            System.err.println(e.getMessage());
+        }
+    }
+    
+    public void insertarBD(int id, String nom, String pregunta, int idSI, int idNO) {
+        System.out.printf("insertarBD. id rebut %d\n", id);
+        var url = URL;
+        var sql = "INSERT INTO animals(id, nom, pregunta, idSI, idNO) VALUES(?,?,?,?,?)";
+        
+        try( var conn = DriverManager.getConnection(url);
+             var stmt = conn.prepareStatement(sql) ) {
+            
+            stmt.setInt(1, id);
+            stmt.setString(2, nom);
+            stmt.setString(3, pregunta);
+            stmt.setInt(4, idSI);
+            stmt.setInt(5, idNO);
+            
+            stmt.executeUpdate();
+        }
+        catch (SQLException e) {
+            System.err.println(e.getMessage()); 
+        }
+    
+    }
+    
+    public void actualitzarBD(int id, String nom, int idSI, int idNO) {
+        System.out.printf("actualitzarBD. id rebut %d\n", id);
+        var url = URL;
+        var sql = "UPDATE animals SET nom = ? , "
+                + "idSI = ? , idNO = ? "
+                + "WHERE id = ?";
+        
+        try( var conn = DriverManager.getConnection(url);
+             var stmt = conn.prepareStatement(sql) ) {
+            
+            stmt.setString(1, nom);
+            stmt.setInt(2, idSI);
+            stmt.setInt(3, idNO);
+            stmt.setInt(4, id);
+            
+            stmt.executeUpdate();
+        }
+        catch (SQLException e) {
+            System.err.println(e.getMessage()); 
+        }
+        
+    }
+    
+    public int quantsAnimals() {
+        System.out.println("quantsAnimals.");
+        var url = URL;
+        var sql = "SELECT max(id) FROM animals ";
+        
+        try( var conn = DriverManager.getConnection(url);
+             var stmt = conn.prepareStatement(sql) ) {
+
+            var rs = stmt.executeQuery() ;
+            return rs.getInt(1);
+        }
+        catch (SQLException e) {
+            System.err.println(e.getMessage()); 
+            return 0;
+        }
+        
+    }
 }
